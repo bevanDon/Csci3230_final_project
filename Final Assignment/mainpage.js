ee490624d837635bc2d17cb64a416972058c50fb
bevan Donbosco <bevan2424@hotmail.com>
2019-03-27 19:42:38 -0400

    starting to create the database server

---
+++ Final Assignment/mainpage.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#submit").click( function() {
+        var MongoClient = require('mongodb').MongoClient;
+        var url = "mongodb://localhost:27019/mydb";
+     
+      var user_id = parseFloat($("#email").val());
+      var password = parseFloat($("#password").val());
+    
+  
+
+    
+    })
+  })
+  
