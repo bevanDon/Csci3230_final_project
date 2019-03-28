6cc6b03a01a462c25a488231520a59175928e20e
bevan Donbosco <bevan2424@hotmail.com>
2019-03-27 22:58:27 -0400

    bug fixing for jquery json parsing

--- Final Assignment/mainpage.js
+++ Final Assignment/mainpage.js
@@ -1,26 +1,34 @@
+$.support.cors = true;
 $(document).ready(function() {
-    $("#submit").click( function() {
-        var mongoose = require('mongoose');
-        var mongoDB = 'mongodb://127.0.0.1/university';
-        mongoose.connect(mongoDB, { useNewUrlParser: true });
-var db = mongoose.connection;
-var login = new Schema({
-    sid: {type: String, 
-             validate: [/^1[0­9]{8}$/, 'must be be a valid email'],
-             unique: true,
-             index: true},
-    Username: String,
-    Password: String,
-    }, {collection: 'Users'});
-    
+    var schedule_results= $("schedule_results");
+    var scores;
+    $("#results").hide();
+    $("#schedule").click(function() {
+        $("#myVideo").hide();
+        $("#results").show();
+        
+        
+        $.getJSON("scores.json", function(response) {
+            scores = response["rounds"];
+            
+            
+            $(scores).each(function() {
+                matches = this["matches"];
+                $(matches).each(function() {
+                var div = $("<div>")
+                div.addClass("final_results");
+                var content = $("<div>");
 
-     
-      var user_id = parseFloat($("#email").val());
-      var password = parseFloat($("#password").val());
-    
-  
+                content.addClass("result_data");
+                content.append("<h4><b>" + this["date"] + "</b></h4>")
+                content.append("<p>" + this["team1"]["code"]  + this["score1"] +
+                this["score2"]  + this["team2"]["code"] + "<br>"
+                + "</p>")
+                schedule_results.append(content);
 
-    
-    })
-  })
-  
+                })
+            })
+        });
+    });
+});
+
