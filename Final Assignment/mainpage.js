abfdb3c45e94d01977d78394c09d142f8654e3fa
bevan Donbosco <bevan2424@hotmail.com>
2019-03-27 20:17:29 -0400

    Using mongoDB to create and save users

--- Final Assignment/mainpage.js
+++ Final Assignment/mainpage.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
     $("#submit").click( function() {
-        var MongoClient = require('mongodb').MongoClient;
-        var url = "mongodb://localhost:27019/mydb";
+        var mongoose = require('mongoose');
+        var mongoDB = 'mongodb://127.0.0.1/university';
+        mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+var login = new Schema({
+    sid: {type: String, 
+             validate: [/^1[0­9]{8}$/, 'must be be a valid email'],
+             unique: true,
+             index: true},
+    Username: String,
+    Password: String,
+    }, {collection: 'Users'});
+    
+
      
       var user_id = parseFloat($("#email").val());
       var password = parseFloat($("#password").val());
